2042ed4ac3ec43d181661f86a1aa6ecab7f17f90
olek <olek.serediuk@gmail.com>
2017-08-22 09:36:04 +0200

    A lot of small changes v2

--- src/main/java/demo/ScrapperDemo.java
+++ src/main/java/demo/ScrapperDemo.java
@@ -6,7 +6,7 @@ public class ScrapperDemo {
 
     public static void main(String[] args)  {
         IPKOScrapper scrapper = new IPKOScrapper();
-        scrapper.authenticate("58139759", "156489Vert!@");
+        scrapper.authenticate("58139759", "--");
         scrapper.fetchAccountList().stream().forEach(System.out::println);
     }
 
